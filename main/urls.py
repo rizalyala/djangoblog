04e6430f46c4d3788b816b8bfd4616187d2f03ff
rizalyala <rizalyala19@gmail.com>
2025-06-18 12:42:01 +0000

    detailing homepage

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('list/', views.blog_list, name='blog_list'),
     path('post/<int:post_id>/', views.blog_detail, name='blog_detail'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def homepage(request):
-    posts = Post.objects.all().order_by('-created_at')
+    posts = Post.objects.all().order_by('-created_at')[:15]
     return render(request, 'index.html', {'posts': posts})
+
+def blog_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'list.html', {'posts': posts})
 
 def blog_detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
